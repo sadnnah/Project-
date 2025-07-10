fda92e3c8608434b89d3278632dfb73efe52e3fc
sadnnah <sadnnahsaisantosh@gmail.com>
2025-07-10 18:58:38 +0100

    created the python code file

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def get_db():
     conn.row_factory = sqlite3.Row
     return conn
 
-
 with get_db() as db:
     db.execute('''
         CREATE TABLE IF NOT EXISTS cards (
@@ -30,3 +29,73 @@ with get_db() as db:
             status TEXT NOT NULL
         )
     ''')
+
+def generate_card_number():
+    return ''.join([str(random.randint(0,9)) for _ in range(12)])
+
+def get_dates():
+    today = datetime.now().date()
+    expiry = today + timedelta(days=365*10)
+    return today.isoformat(), expiry.isoformat()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/cards', methods=['GET'])
+def get_cards():
+    db = get_db()
+    cards = db.execute('SELECT * FROM cards').fetchall()
+    return jsonify([dict(card) for card in cards])
+
+@app.route('/api/cards', methods=['POST'])
+def add_card():
+    data = request.json
+    card_number = generate_card_number()
+    issue_date, expiry_date = get_dates()
+    db = get_db()
+    db.execute('''
+        INSERT INTO cards (first_name, last_name, nationality, passport_number, employment_type, card_number, issue_date, expiry_date, status)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (
+        data['first_name'],
+        data['last_name'],
+        data['nationality'],
+        data['passport_number'],
+        data['employment_type'],
+        card_number,
+        issue_date,
+        expiry_date,
+        data['status']
+    ))
+    db.commit()
+    return jsonify({'message': 'Application submitted!', 'card_number': card_number}), 201
+
+@app.route('/api/cards/<int:card_id>', methods=['PUT'])
+def update_card(card_id):
+    data = request.json
+    db = get_db()
+    db.execute('''
+        UPDATE cards SET first_name=?, last_name=?, nationality=?, passport_number=?, employment_type=?, status=?
+        WHERE id=?
+    ''', (
+        data['first_name'],
+        data['last_name'],
+        data['nationality'],
+        data['passport_number'],
+        data['employment_type'],
+        data['status'],
+        card_id
+    ))
+    db.commit()
+    return jsonify({'message': 'Application updated!'})
+
+@app.route('/api/cards/<int:card_id>', methods=['DELETE'])
+def delete_card(card_id):
+    db = get_db()
+    db.execute('DELETE FROM cards WHERE id=?', (card_id,))
+    db.commit()
+    return jsonify({'message': 'Application deleted!'})
+
+if __name__ == '__main__':
+    app.run(debug=True) 
